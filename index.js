b2d8fc09bc7ba8f50925ad07c4e7f4acb2d0b390
rachela1 <rachel_kaitlin01@hotmail.com>
2023-06-19 20:11:31 -0500

    created files for shapes and added shape data

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ prompt([
     {
      type: 'input',
      message: 'Choose the text color (color keyword OR a hexadecimal number).',
-     name: 'text color',
+     name: 'textColor',
     },
     {
      type: 'list',   
@@ -24,8 +24,14 @@ prompt([
     {
      type: 'input',
      message: 'Choose the shape color (color keyword OR a hexadecimal number).',
-     name: 'shape color',
+     name: 'shapeColor',
     },
-]).then((answers) =>{
+]).then((answers) => {
+    if (answers.text.length > 3) {
+        console.log("No more than 3 characters, please.");
+        promptUser();
+    } else {
+        writeToFile('logo.svg', answers);
+    }
     console.table(answers);
 });
