d1f71d34a69e1067edadc8afeb0c019b2c1c53a3
rachela1 <rachel_kaitlin01@hotmail.com>
2023-06-27 23:04:51 -0500

    added code to shapes.test.js and added info to README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# logo-maker
+# Logo Maker
+
+## Description
+This is an application that will generate a three letter logo with 3 different shape options. With text and shape color options also!
+
+## Installation
+To use this application copy the repository to your terminal and type npm install. 
+
+## Usage
+To start the application simply type node index in your terminal. After you have typed that you will be prompted to answer questions based on how you wish your logo to look. 
+
+[Video Guide](https://drive.google.com/drive/folders/1MdNHyh215CTY_z5PtQCAkxg7FlzP6mFn?usp=sharing)
--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -3,11 +3,51 @@ var testCases = [
 {
     description:"expected to throw error if input is empty.",
     input: {},
-    expectedErr: new Error('Must include input.'),
+    expectedErr: new Error('Must include input.')
 },
 {
     description:"expected to throw error if more than 3 characters.",
     input: {logoCharacters:'four'},
-    expectedErr: new Error('Must be less than 3 characters.'),
+    expectedErr: new Error('Must be less than 3 characters.')
+},
+{
+    description:"expected to throw error if input is not a valid css color",
+    input: {logoCharacters:'Fun', textColor: 'noColor'},
+    expectedErr: new Error('Must be valid hexidecimal code or css color.')
+},
+{
+    description:"expected to throw error if render() is called.",
+    input:{logoCharacters:'Fun', textColor:'pink', shapeColor:'white'},
+    expectedErr: new Error ('render method required for child shapes.'),
+    shouldRender:true
+},
+{
+    description:"expected to add shape color if it is a valid color",
+    input:{ logoCharacters: 'Fun', textColor:'pink', shapeColor:'white'},
+    expectedKey:'shapeColor',
+    expectedValue: 'white'
+},
+{
+    description:"expected to add textcolor if it is a valid color",
+    input:{ logoCharacters:'Fun', textColor:'pink', shapeColor:'white'},
+    expectedKey: "textColor",
+    expectedValue: 'white'
 },
 ]
+
+describe("Shape test suite", () => {
+    for (let tc of testCases) {
+      it(tc.desc, () => {
+        if (tc.shouldRender) {
+          const shape = new Shape(tc.input);
+          expect(shape.render).toThrow(tc.expectedErr);
+        } else if (tc.expectedErr) {
+          const shape = () => new Shape(tc.input);
+          expect(shape).toThrow(tc.expectedErr);
+        } else {
+          const shape = new Shape(tc.input);
+          expect(shape[tc.expectedKey]).toBe(tc.expectedValue);
+        }
+      });
+    }
+  });
